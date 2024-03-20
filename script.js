cf35e34a7f9daa896b97533cdf42d560c027e4bc
Mayza Ester <130400582+maisazs@users.noreply.github.com>
2024-03-20 16:03:24 -0300

    att

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function abrirOpt() {
-    let opt = document.getElementsByClassName('opt');
-
-    if (opt[1].classList.contains('.fechado')) {
-        opt[1].classList.add('aberto');
-        opt[1].classList.remove('fechado');
+function abrirOpt(thing, categ) {
+    if (thing.classList.contains('fechado')) {
+        thing.classList.remove('fechado');
+        thing.classList.add('aberto');
+        categ.classList.add('fechado');
+    } else {
+        thing.classList.add('fechado');
+        thing.classList.remove('aberto');
+        categ.classList.add('aberto');
+        categ.classList.remove('fechado');
     }
 }
